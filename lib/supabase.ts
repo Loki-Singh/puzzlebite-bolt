9ecc5f73a6db5493e4484cfe18e927fa359330a4
Loki-Singh <142884055+Loki-Singh@users.noreply.github.com>
2025-11-09 19:58:43 +0530

    Updated supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,11 +4,11 @@ import Constants from 'expo-constants';
 
 const supabaseUrl =
   Constants.expoConfig?.extra?.EXPO_PUBLIC_SUPABASE_URL ||
-  'https://0ec90b57d6e95fcbda19832f.supabase.co';
+  'https://wdanjczadkgcoukmsggn.supabase.co';
 
 const supabaseAnonKey =
   Constants.expoConfig?.extra?.EXPO_PUBLIC_SUPABASE_ANON_KEY ||
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJib2x0IiwicmVmIjoiMGVjOTBiNTdkNmU5NWZjYmRhMTk4MzJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4ODE1NzQsImV4cCI6MTc1ODg4MTU3NH0.9I8-U0x86Ak8t2DGaIk0HfvTSLsAyzdnz-Nw00mMkKw';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkYW5qY3phZGtnY291a21zZ2duIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2OTYxODYsImV4cCI6MjA3ODI3MjE4Nn0._9C3dOdUt3CLkJ-NWYuayfeJfNh3lh__7P1cR0D4p5A';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
