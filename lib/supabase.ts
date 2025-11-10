6fe7a0831baf67da1b78a14336ea6733d1e73c97
Loki-Singh <142884055+Loki-Singh@users.noreply.github.com>
2025-11-10 12:09:08 +0530

    Updated supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,14 +1,8 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
-import Constants from 'expo-constants';
 
-const supabaseUrl =
-  Constants.expoConfig?.extra?.EXPO_PUBLIC_SUPABASE_URL ||
-  'https://wdanjczadkgcoukmsggn.supabase.co';
-
-const supabaseAnonKey =
-  Constants.expoConfig?.extra?.EXPO_PUBLIC_SUPABASE_ANON_KEY ||
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkYW5qY3phZGtnY291a21zZ2duIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2OTYxODYsImV4cCI6MjA3ODI3MjE4Nn0._9C3dOdUt3CLkJ-NWYuayfeJfNh3lh__7P1cR0D4p5A';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://wdanjczadkgcoukmsggn.supabase.co';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkYW5qY3phZGtnY291a21zZ2duIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2OTYxODYsImV4cCI6MjA3ODI3MjE4Nn0._9C3dOdUt3CLkJ-NWYuayfeJfNh3lh__7P1cR0D4p5A';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
